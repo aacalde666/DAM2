1637d5ee5b54981b4a70f2ed7eee8a1371e34fb3
aacalde666 <aacal666.social@gmail.com>
2025-01-15 12:42:15 +0100

    Acceso a Datos

---
+++ DAM2/Ejemplo-ORM_HIBERNATE/src/main/java/connection/ConexionBD.java
@@ -0,0 +1,26 @@
+package connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import properties.Properties;
+//CLASEN CON PATRON DE DISEÑO SINGLETON
+public class ConexionBD {
+	private static Connection conex = null;
+	
+	public static Connection getConex() {
+		if (conex != null) {
+			return conex;
+		}
+		String url = Properties.getConfig().getProperty("url");
+		String user = Properties.getConfig().getProperty("user");
+		String passwd = Properties.getConfig().getProperty("passwd");
+		try {
+			conex = DriverManager.getConnection(url, user, passwd);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conex;
+	}
+}
